d43259f42456b914338ae5f9cec6d9570673dc11
e3duardo <e3duardo@gmail.com>
2020-03-01 17:22:56 +0000

    Deploying to gh-pages from master - 2a8c76c94f9b7ef8956cd0e26073fed71d1b1d2d 🚀

---
+++ precache-manifest.81ca260424cf80773731bbba512e4d15.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "908db539a8319c606a5ee09dd40bb8f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ed87a2faddf78be4e97",
+    "url": "/static/js/2.e81d81ee.chunk.js"
+  },
+  {
+    "revision": "2d4e50db9bebc1822939159016933eb5",
+    "url": "/static/js/2.e81d81ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "829632007f25bcaef641",
+    "url": "/static/js/main.0caebc90.chunk.js"
+  },
+  {
+    "revision": "b2a32fb569d1132fe613",
+    "url": "/static/js/runtime-main.ea2547db.js"
+  },
+  {
+    "revision": "4b88321557e8e33ae95a8ae4bda2bd98",
+    "url": "/static/media/clock.4b883215.mp3"
+  },
+  {
+    "revision": "fc814a01726700313d2b1f91276207a7",
+    "url": "/static/media/pause.fc814a01.svg"
+  },
+  {
+    "revision": "f70f50987f3dab19557e0fe23d14ea57",
+    "url": "/static/media/resume.f70f5098.svg"
+  }
+]);
